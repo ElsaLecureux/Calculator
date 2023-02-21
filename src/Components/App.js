85ba77fb63e4df03fbe2d26753c8d941a80cb24c
ElsaLecureux <elsa.lecureux@gmail.com>
2023-02-21 12:03:32 +0100

    simple addition with 2 digits, missing clearing

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,15 +3,46 @@ import { useState } from 'react';
 
 function App() {
   const [screenValue, setScreenValue] = useState('');
-  const addSign = (sign) => {
-    setScreenValue(screenValue + sign);
+  const [operator, setOperator] = useState('');
+  const [firstNumber, setFirstNumber] = useState(0);
+  const [secondNumber, setSecondNumber] = useState(0);
+  
+
+  const addSign = (value) => {
+    setOperator(value)
+    setScreenValue(screenValue + value);
   }
-  const addNumber = (event) => {
-    const number = Number(event);
-    setScreenValue(screenValue + number);
+  const addNumber = (value) => {
+    if (operator === '') {
+      setFirstNumber(Number(firstNumber + value));
+      console.log(firstNumber);
+    } else {
+      setSecondNumber(Number(secondNumber + value));
+    }
+    setScreenValue(screenValue + value);
   }
   const calculation = () => {
-
+    const calc = operator;
+    switch(calc) {
+      case '+':
+        const result = firstNumber + secondNumber;
+        setScreenValue(result);
+        break;
+      case '-':
+        console.log('-');
+        break;
+      case 'X':
+        console.log('X');
+        break;
+      case '&#247;':
+        console.log('&#247;');
+        break;
+        default:
+          console.log('no Math')
+    }
+  }
+  const clearAll = () => {
+    setScreenValue('');
   }
   return (
     <div className="App">
@@ -20,7 +51,7 @@ function App() {
           <p className="screen_content">{screenValue}</p>
         </div>
         <div className="clean_container">
-          <button className="cleaning_button">Clear</button>
+          <button className="cleaning_button" onClick={clearAll}>Clear</button>
           <button className="cleaning_button back_button">&lt;</button>
         </div>
         <div className="digits-symbols_container">
@@ -35,10 +66,10 @@ function App() {
           <button value="1" className="digits_button" onClick={e => addNumber(e.target.value)}>1</button>
           <button value="2" className="digits_button" onClick={e => addNumber(e.target.value)}>2</button>
           <button value="3" className="digits_button" onClick={e => addNumber(e.target.value)}>3</button>
-          <button value="*" className="digits_button" onClick={e => addSign(e.target.value)}>X</button>
+          <button value="X" className="digits_button" onClick={e => addSign(e.target.value)}>X</button>
           <button value="0" className="digits_button" onClick={e => addNumber(e.target.value)}>0</button>
           <button value="." className="digits_button" onClick={e => addSign(e.target.value)}>.</button>
-          <button value="/" className="digits_button" onClick={e => addSign(e.target.value)}>&#247;</button>
+          <button value="&#247;" className="digits_button" onClick={e => addSign(e.target.value)}>&#247;</button>
           <button className="digits_button" onClick={calculation}>=</button>
         </div>      
      </div>
